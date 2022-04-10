8dd609a1cddaf5fdd52e809bd64af51a92aa9b2d
esfandiarijahromi <51745434+esfandiarijahromi@users.noreply.github.com>
2022-04-10 16:36:31 +0430

    Create procedure.sql

---
+++ procedure.sql
@@ -0,0 +1,11 @@
+DECLARE
+   CURSOR feit_cursor IS SELECT * FROM fact_feit_contract;
+
+BEGIN
+  for feit_item in feit_cursor
+  LOOP
+      begin
+      feit_monthly( feit_item.terminalserialid, feit_item.marketerid , feit_item.FSDATE,feit_item.FEDATE,feit_item.CSDATE,feit_item.CEDATE);
+      end;
+  END LOOP;
+END;
