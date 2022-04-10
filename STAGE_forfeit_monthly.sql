d7597be6f26cca88241b01ed66ece12a4e58451a
esfandiarijahromi <51745434+esfandiarijahromi@users.noreply.github.com>
2022-04-10 16:35:57 +0430

    Create STAGE_forfeit_monthly.sql

---
+++ STAGE_forfeit_monthly.sql
@@ -0,0 +1,9 @@
+drop table STAGE_forfeit_monthly purge
+create table STAGE_forfeit_monthly
+as
+SELECT /*+parallel(source 4) nologging */ t.* , 
+(SELECT /*+parallel(source 4) nologging */ COUNT(*) FROM b s 
+               WHERE s.Terminal = t.TerminalSerialId AND 
+                     s.TrDate BETWEEN t.PreDateTime AND t.CurrentDateTime 
+                     ) Tr_count
+FROM a t
